a04f8e4382dec150e902340c40b053b6777c11cc
Elliot Chance <elliotchance@gmail.com>
2014-05-06 14:12:52 +1000

    testSettingDataWhenCreatingAssertion

--- tests/Concise/AssertionTest.php
+++ tests/Concise/AssertionTest.php
@@ -9,10 +9,16 @@ class AssertionTest extends TestCase
 		$assertion = new Assertion('some string');
 		$this->assertEquals('some string', $assertion->getAssertion());
 	}
-	
+
 	public function testCreatingAssertionWithoutProvidingDataIsAnEmptyArray()
 	{
 		$assertion = new Assertion('some string');
 		$this->assertEquals(array(), $assertion->getData());
+	}
+
+	public function testSettingDataWhenCreatingAssertion()
+	{
+		$assertion = new Assertion('some string', array('abc', 'def'));
+		$this->assertEquals(array('abc', 'def'), $assertion->getData());
 	}
 }
