00bb8c0b955225609bdd1ce47544050a18a95b6b
Elliot Chance <elliotchance@gmail.com>
2014-05-07 22:01:52 +1000

    testTypeIsRequiredByConstructor

---
+++ src/Concise/Token.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Concise;
+
+class Token
+{
+	protected $type;
+
+	public function __construct($type)
+	{
+		$this->type = $type;
+	}
+
+	public function getType()
+	{
+		return $this->type;
+	}
+}
---
+++ tests/Concise/TokenTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Concise;
+
+class TokenTest extends TestCase
+{
+	public function testTypeIsRequiredByConstructor()
+	{
+		$attribute = new Token(Lexer::TOKEN_STRING);
+		$this->assertEquals(Lexer::TOKEN_STRING, $attribute->getType());
+	}
+}
