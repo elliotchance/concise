f9cf034c5742a52bc5603128ff9e9d74c5adf935
Elliot Chance <elliotchance@gmail.com>
2014-05-08 10:18:24 +1000

    More string escape sequences

--- src/Concise/Lexer.php
+++ src/Concise/Lexer.php
@@ -14,10 +14,6 @@ class Lexer
 
 	const TOKEN_STRING = 5;
 
-	// @test TOKEN_STRING with escaped quotes
-
-	// @test TOKEN_STRING with escaped characters
-
 	// @test keywords should be generated automatically from matchers
 	protected static $keywords = array(
 		'equal',
@@ -31,11 +27,28 @@ class Lexer
 
 	protected static function convertEscapedCharacter($ch)
 	{
+		// @test \cx : "control-x", where x is any character
+		// @test \p{xx} : a character with the xx property, see unicode properties for more info
+		// @test \P{xx} : a character without the xx property, see unicode properties for more info
+		// @test \xhh : character with hex code hh
+		// @test \ddd : character with octal code ddd, or backreference
+		if($ch === 'a') {
+			return "\a";
+		}
+		if($ch === 'e') {
+			return "\e";
+		}
+		if($ch === 'f') {
+			return "\f";
+		}
 		if($ch === 'n') {
 			return "\n";
 		}
 		if($ch === 'r') {
 			return "\r";
+		}
+		if($ch === 't') {
+			return "\t";
 		}
 	}
 
