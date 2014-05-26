94c26e5fda6b10099dbba4fc6d26bf12cb15c30c
Elliot Chance <elliotchance@gmail.com>
2014-05-26 23:51:49 +1000

    testExpectFailures

--- src/Concise/Matcher/Throws.php
+++ src/Concise/Matcher/Throws.php
@@ -17,17 +17,36 @@ class Throws extends AbstractMatcher
 		if(!is_callable($data[0])) {
 			throw new DidNotMatchException("The attribute to test for exception must be callable (an anonymous function)");
 		}
-		$r = false;
-		try {
-			$data[0]();
+
+		if('? throws ?' === $syntax) {
+			try {
+				$data[0]();
+			}
+			catch(\Exception $exception) {
+				$exceptionClass = get_class($exception);
+				$matchesOrIsSubclassOf = ($exceptionClass === $data[1]) || is_subclass_of($exception, $data[1]);
+				if(!$matchesOrIsSubclassOf) {
+					throw new DidNotMatchException("Expected {$data[1]} to be thrown, but $exceptionClass was thrown.");
+				}
+				return true;
+			}
+			throw new DidNotMatchException("Expected {$data[1]} to be thrown, but nothing was thrown.");
 		}
-		catch(\Exception $exception) {
-			$r = (get_class($exception) === $data[1]) || is_subclass_of($exception, $data[1]);
-		}
-		
+
 		if('? does not throw ?' === $syntax) {
-			$r = !$r;
+			try {
+				$data[0]();
+			}
+			catch(\Exception $exception) {
+				$exceptionClass = get_class($exception);
+				$matchesOrIsSubclassOf = ($exceptionClass === $data[1]) || is_subclass_of($exception, $data[1]);
+				if($matchesOrIsSubclassOf) {
+					throw new DidNotMatchException("Expected {$data[1]} to be thrown, but $exceptionClass was thrown.");
+				}
+			}
+			return true;
 		}
-		return $r;
+
+		return true;
 	}
 }
--- tests/Concise/Matcher/ThrowsTest.php
+++ tests/Concise/Matcher/ThrowsTest.php
@@ -4,11 +4,11 @@ namespace Concise\Matcher;
 
 use \Concise\TestCase;
 
-class MyCustomException extends \Exception
+class MyException extends \Exception
 {
 }
 
-class MyOtherException extends \Exception
+class OtherException extends \Exception
 {
 }
 
@@ -23,16 +23,38 @@ class ThrowsTest extends AbstractMatcherTestCase
 	public function _test_comparisons()
 	{
 		$this->doesThrow = function() {
-			throw new MyCustomException();
+			throw new MyException();
 		};
 		$this->doesThrowOther = function() {
-			throw new MyOtherException();
+			throw new OtherException();
 		};
 		return array(
-			'doesThrow throws \Concise\Matcher\MyCustomException',
 			'doesThrow throws \Exception',
-			'doesThrow does not throw \Concise\Matcher\MyOtherException',
+			'doesThrow throws \Concise\Matcher\MyException',
+			'doesThrow does not throw \Concise\Matcher\OtherException',
 		);
+	}
+
+	public function expectFailures()
+	{
+		return array(
+			array(function() {}, '\Exception')
+		);
+	}
+
+	/**
+	 * @dataProvider expectFailures
+	 */
+	public function testExpectFailures(callable $method, $expectedException)
+	{
+		try {
+			$this->matcher->match('? throws ?', array($method, $expectedException));
+		}
+		catch(DidNotMatchException $e) {
+			$this->assertTrue(true);
+			return;
+		}
+		$this->fail("Exception not thrown.");
 	}
 
 	/**
