91ced8f9d89022be7edf9d880be0851e411fd81d
Elliot Chance <elliotchance@gmail.com>
2014-05-31 12:17:20 +1000

    testAllNonspecificComparisonsUseConvertToString

--- src/Concise/Services/Comparer.php
+++ src/Concise/Services/Comparer.php
@@ -4,8 +4,17 @@ namespace Concise\Services;
 
 class Comparer
 {
+	protected $convertToString;
+
+	public function setConvertToString(\Concise\Services\ConvertToString $convertToString)
+	{
+		$this->convertToString = $convertToString;
+	}
+
 	public function compare($a, $b)
 	{
+		$a = $this->convertToString->convertToString($a);
+		$b = $this->convertToString->convertToString($b);
 		return $a === $b;
 	}
 }
--- tests/Concise/Services/ComparerTest.php
+++ tests/Concise/Services/ComparerTest.php
@@ -4,15 +4,16 @@ namespace Concise\Services;
 
 class ComparerTest extends \Concise\TestCase
 {
-	public function testTrueEqualsTrue()
+	// @test getMock wont behave correctly unless inside a running test
+	public function testAllNonspecificComparisonsUseConvertToString()
 	{
-		$comparer = new Comparer();
-		$this->assertTrue($comparer->compare(true, true));
-	}
+		$convertToStringMock = $this->getMock('\Concise\Services\ConvertToString');
+		$convertToStringMock->expects($this->exactly(2))
+		                    ->method('convertToString')
+		                    ->will($this->returnValue(true));
 
-	public function testTrueDoesNotEqualFalse()
-	{
-		$comparer = new Comparer();
-		$this->assertFalse($comparer->compare(true, false));
+		$this->comparer = new Comparer();
+		$this->comparer->setConvertToString($convertToStringMock);
+		$this->assertTrue($this->comparer->compare("abc", "abc"));
 	}
 }
