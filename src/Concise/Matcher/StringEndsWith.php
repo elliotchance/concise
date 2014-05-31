f1296b3546247584ddacf13bcc380a554dcbe53c
Elliot Chance <elliotchance@gmail.com>
2014-05-31 16:07:04 +1000

    Refactoring

--- src/Concise/Matcher/StringEndsWith.php
+++ src/Concise/Matcher/StringEndsWith.php
@@ -2,26 +2,26 @@
 
 namespace Concise\Matcher;
 
+use \Concise\Services\ConvertToString;
+
 class StringEndsWith extends AbstractMatcher
 {
 	public function supportedSyntaxes()
 	{
 		return array(
 			'? ends with ?',
-			'? does not end with ?'
+			'? does not end with ?',
 		);
-	}
-
-	protected function performMatch(array $data = array())
-	{
-		$haystack = (string) $data[0];
-		$needle = (string) $data[1];
-		return substr($haystack, strlen($haystack) - strlen($needle)) == $needle;
 	}
 
 	public function match($syntax, array $data = array())
 	{
-		$match = $this->performMatch($data);
+		$converter = new ConvertToString();
+		$haystack = $converter->convertToString($data[0]);
+		$needle = $converter->convertToString($data[1]);
+
+		$match = (substr($haystack, strlen($haystack) - strlen($needle)) === $needle);
+
 		if($syntax === '? does not end with ?') {
 			$match = !$match;
 		}
--- src/Concise/Matcher/StringStartsWith.php
+++ src/Concise/Matcher/StringStartsWith.php
@@ -2,6 +2,8 @@
 
 namespace Concise\Matcher;
 
+use \Concise\Services\ConvertToString;
+
 class StringStartsWith extends AbstractMatcher
 {
 	public function supportedSyntaxes()
@@ -12,16 +14,13 @@ class StringStartsWith extends AbstractMatcher
 		);
 	}
 
-	protected function performMatch(array $data = array())
-	{
-		$haystack = (string) $data[0];
-		$needle = (string) $data[1];
-		return substr($haystack, 0, strlen($needle)) == $needle;
-	}
-
 	public function match($syntax, array $data = array())
 	{
-		$match = $this->performMatch($data);
+		$converter = new ConvertToString();
+		$haystack = $converter->convertToString($data[0]);
+		$needle = $converter->convertToString($data[1]);
+
+		$match = (substr($haystack, 0, strlen($needle)) === $needle);
 		if($syntax === '? does not start with ?') {
 			$match = !$match;
 		}
