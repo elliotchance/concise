206860e0bc0261974338886681300ad9068fedd0
Elliot Chance <elliotchance@gmail.com>
2014-06-04 22:24:01 +1000

    testDescriptionOfInteger

--- src/Concise/Services/ValueDescriptor.php
+++ src/Concise/Services/ValueDescriptor.php
@@ -6,6 +6,9 @@ class ValueDescriptor
 {
 	public function describe($value)
 	{
+		if(is_int($value)) {
+			return 'integer';
+		}
 		return 'string';
 	}
 }
--- tests/Concise/Services/ValueDescriptorTest.php
+++ tests/Concise/Services/ValueDescriptorTest.php
@@ -4,9 +4,19 @@ namespace Concise\Services;
 
 class ValueDescriptorTest extends \Concise\TestCase
 {
+	public function prepare()
+	{
+		parent::prepare();
+		$this->descriptor = new ValueDescriptor();
+	}
+
 	public function testDescriptionOfString()
 	{
-		$this->descriptor = new ValueDescriptor();
 		$this->assertSame('string', $this->descriptor->describe('abc'));
+	}
+
+	public function testDescriptionOfInteger()
+	{
+		$this->assertSame('integer', $this->descriptor->describe(123));
 	}
 }
