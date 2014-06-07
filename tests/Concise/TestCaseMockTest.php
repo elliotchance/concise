637117324afd51207ef24be0d7e40bea37ada676
Elliot Chance <elliotchance@gmail.com>
2014-06-07 14:24:15 +1000

    _test_can_use_mock

--- tests/Concise/TestCaseMockTest.php
+++ tests/Concise/TestCaseMockTest.php
@@ -12,4 +12,13 @@ class TestCaseMockTest extends TestCase
 		     ->will($this->returnValue(123));
 		$this->assertSame(123, $mock->mymethod());
 	}
+
+	public function _test_can_use_mock()
+	{
+		$this->mock = $this->getMock('\stdClass', array('mymethod'));
+		$this->mock->expects($this->any())
+		           ->method('mymethod')
+		           ->will($this->returnValue(456));
+		return '`$self->mock->mymethod()` equals 456';
+	}
 }
