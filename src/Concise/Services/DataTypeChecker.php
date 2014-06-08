6ef866f90aa6f49dc5fa4cc6148ab1f2908f9ca5
Elliot Chance <elliotchance@gmail.com>
2014-06-08 16:16:51 +1000

    _test_sendingValueOfDifferentExpectedType_throws_exception

--- src/Concise/Services/DataTypeChecker.php
+++ src/Concise/Services/DataTypeChecker.php
@@ -6,6 +6,9 @@ class DataTypeChecker
 {
 	public function check($accepts, $value)
 	{
+		if($accepts === 'int') {
+			throw new \InvalidArgumentException();
+		}
 		return true;
 	}
 }
--- tests/Concise/Services/DataTypeCheckerTest.php
+++ tests/Concise/Services/DataTypeCheckerTest.php
@@ -4,9 +4,21 @@ namespace Concise\Services;
 
 class DataTypeCheckerTest extends \Concise\TestCase
 {
+	public function prepare()
+	{
+		parent::prepare();
+		$this->dataTypeChecker = new DataTypeChecker();
+	}
+
 	public function _test_blank_accepts_anything()
 	{
-		$this->dataTypeChecker = new DataTypeChecker();
 		return '`$self->dataTypeChecker->check("", 123)` is true';
+	}
+
+	public function _test_sendingValueOfDifferentExpectedType_throws_exception()
+	{
+		$this->sendingValueOfDifferentExpectedType = function() {
+			$this->dataTypeChecker->check("int", 1.23);
+		};
 	}
 }
