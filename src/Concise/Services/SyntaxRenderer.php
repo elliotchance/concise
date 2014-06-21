8acbeadf86e1e1aea204d58ae36a81df762015bb
Elliot Chance <elliotchance@gmail.com>
2014-06-21 13:00:11 +1000

    Unused variable

--- src/Concise/Services/SyntaxRenderer.php
+++ src/Concise/Services/SyntaxRenderer.php
@@ -7,7 +7,7 @@ class SyntaxRenderer
 	public function render($syntax, array $data = array())
 	{
 		$renderer = new ValueRenderer();
-		return preg_replace_callback('/\?/', function($match) use(&$data, $renderer) {
+		return preg_replace_callback('/\?/', function() use(&$data, $renderer) {
     		$r = $renderer->render(array_shift($data));
     		return $r;
 		}, $syntax);
