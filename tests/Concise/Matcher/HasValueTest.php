bcd038255570db2b0b92f10689b7129740ed3076
Elliot Chance <elliotchance@gmail.com>
2014-06-22 22:15:26 +1000

    testArrayValueExists

---
+++ tests/Concise/Matcher/HasValueTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Concise\Matcher;
+
+class HasValueTest extends AbstractMatcherTestCase
+{
+	public function setUp()
+	{
+		parent::setUp();
+		$this->matcher = new HasValue();
+	}
+
+	public function testArrayValueExists()
+	{
+		$this->assert('[123] has value 123');
+	}
+}
