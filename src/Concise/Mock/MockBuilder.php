733145c134ca827483f4b639beef1eb6b9179003
Elliot Chance <elliotchance@gmail.com>
2014-06-27 00:09:50 +1000

    testMockCanBeCreatedFromAClassThatExists

--- src/Concise/Mock/MockBuilder.php
+++ src/Concise/Mock/MockBuilder.php
@@ -4,12 +4,17 @@ namespace Concise\Mock;
 
 class MockBuilder
 {
-	public function __construct($className)
+	/** @var \PHPUnit_Framework_TestCase */
+	protected $testCase;
+
+	public function __construct(\PHPUnit_Framework_TestCase $testCase, $className)
 	{
+		$this->testCase = $testCase;
+		$this->className = $className;
 	}
 
 	public function getMock()
 	{
-		return null;
+		return $this->testCase->getMock($this->className);
 	}
 }
--- tests/Concise/Mock/MockBuilderTest.php
+++ tests/Concise/Mock/MockBuilderTest.php
@@ -8,7 +8,7 @@ class MockBuilderTest extends TestCase
 {
 	public function testMockCanBeCreatedFromAClassThatExists()
 	{
-		$generator = new MockBuilder('\Concise\TestCase');
+		$generator = new MockBuilder($this, '\Concise\TestCase');
 		$this->mock = $generator->getMock();
 		$this->assert('mock instance of \Concise\TestCase');
 	}
