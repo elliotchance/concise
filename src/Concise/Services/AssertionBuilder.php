f32cb4c39232d1fad3a160c867142078e7126972
Elliot Chance <elliotchance@gmail.com>
2014-06-30 19:48:52 +1000

    testWillThrowExceptionIfAssertionCannotBeFound

--- src/Concise/Services/AssertionBuilder.php
+++ src/Concise/Services/AssertionBuilder.php
@@ -2,14 +2,32 @@
 
 namespace Concise\Services;
 
+use \Concise\Syntax\MatcherParser;
+
 class AssertionBuilder
 {
+	protected $args;
+
 	public function __construct(array $args)
 	{
+		$this->args = $args;
 	}
 
 	public function getAssertion()
 	{
-		return new \Concise\Matcher\Equals();
+		$syntax = array();
+		$argc = count($this->args);
+		for($i = 0; $i < $argc; ++$i) {
+			if($i % 2 === 0) {
+				$syntax[] = '?';
+			}
+			else {
+				$syntax[] = $this->args[$i];
+			}
+		}
+
+		$matcherParser = MatcherParser::getInstance();
+		$assertion = $matcherParser->compile(implode(' ', $syntax));
+		return $assertion->getMatcher();
 	}
 }
--- tests/Concise/Services/AssertionBuilderTest.php
+++ tests/Concise/Services/AssertionBuilderTest.php
@@ -11,4 +11,14 @@ class AssertionBuilderTest extends TestCase
 		$builder = new AssertionBuilder(array(123, 'equals', 123));
 		$this->assertInstanceOf('\Concise\Matcher\Equals', $builder->getAssertion());
 	}
+
+	/**
+	 * @expectedException \Exception
+	 * @expectedExceptionMessage No such matcher for syntax '? array ?'.
+	 */
+	public function testWillThrowExceptionIfAssertionCannotBeFound()
+	{
+		$builder = new AssertionBuilder(array('foo', 'array', 123));
+		$builder->getAssertion();
+	}
 }
