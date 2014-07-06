b1b04dbb2f7700acced93b124fddd13e0e56fe51
Elliot Chance <elliotchance@gmail.com>
2014-07-06 11:07:01 +1000

    testANumberExistsBetweenTwoOtherNumbers

---
+++ src/Concise/Matcher/Between.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Concise\Matcher;
+
+class Between extends AbstractMatcher
+{
+	const DESCRIPTION = 'A number must be between two values (inclusive).';
+
+	public function supportedSyntaxes()
+	{
+		return array(
+			'?:number is between ?:number and ?:number' => self::DESCRIPTION,
+			'?:number between ?:number and ?:number' => self::DESCRIPTION,
+		);
+	}
+
+	public function match($syntax, array $data = array())
+	{
+		return true;
+	}
+}
---
+++ tests/Concise/Matcher/BetweenTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Concise\Matcher;
+
+use \Concise\TestCase;
+
+class BetweenTest extends AbstractMatcherTestCase
+{
+	public function setUp()
+	{
+		parent::setUp();
+		$this->matcher = new Between();
+	}
+
+	public function testANumberExistsBetweenTwoOtherNumbers()
+	{
+		$this->assert(123, between, 100, 'and', 150);
+	}
+}
