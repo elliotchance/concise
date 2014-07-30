8c84738003bec1502d9b6793e2b8881d8b09ea2c
Elliot Chance <elliotchance@gmail.com>
2014-07-31 08:15:21 +1000

    ContainsStringIgnoringCaseTest

---
+++ src/Concise/Matcher/ContainsStringIgnoringCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Concise\Matcher;
+
+class ContainsStringIgnoringCase extends ContainsString
+{
+	public function supportedSyntaxes()
+	{
+		return array(
+			'?:string contains string ?:string ignoring case' => 'A string contains a substring (ignoring case-sensitivity)',
+		);
+	}
+
+	public function match($syntax, array $data = array())
+	{
+		return parent::match(null, array(strtolower($data[0]), strtolower($data[1])));
+	}
+
+	public function getTags()
+	{
+		return array(Tag::STRINGS);
+	}
+}
