65a1f152a3a93d451eb2888948bc850827decab6
Elliot Chance <elliotchance@gmail.com>
2014-08-05 21:44:51 +1000

    NumberToTimesConverterTest

---
+++ src/Concise/Services/NumberToTimesConverter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Concise\Services;
+
+use \Concise\TestCase;
+
+class NumberToTimesConverter
+{
+	public function convert($number)
+	{
+		return 'once';
+	}
+}
---
+++ tests/Concise/Services/NumberToTimesConverterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Concise\Services;
+
+use \Concise\TestCase;
+
+class NumberToTimesConverterTest extends TestCase
+{
+	public function testOneIsOnce()
+	{
+		$converter = new NumberToTimesConverter();
+		$this->assert($converter->convert(1), equals, 'once');
+	}
+}
