786cbbce5473bd3554a0d7437a035a0f2b730325
Elliot Chance <elliotchance@gmail.com>
2014-08-17 16:45:50 +1000

    testConstructorWillOnlyAcceptAnException

--- src/Concise/Mock/Action/ThrowAction.php
+++ src/Concise/Mock/Action/ThrowAction.php
@@ -4,6 +4,11 @@ namespace Concise\Mock\Action;
 
 class ThrowAction extends AbstractCachingAction
 {
+    public function __construct(\Exception $e)
+    {
+        parent::__construct($e);
+    }
+
     public function getActionCode()
     {
         return parent::getActionCode() . 'throw $v;';
