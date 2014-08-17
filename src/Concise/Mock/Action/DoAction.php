3e2186df607a2be111503899d987d672c143dad5
Elliot Chance <elliotchance@gmail.com>
2014-08-17 16:43:36 +1000

    testConstructorWillOnlyAcceptClosure

--- src/Concise/Mock/Action/DoAction.php
+++ src/Concise/Mock/Action/DoAction.php
@@ -4,6 +4,11 @@ namespace Concise\Mock\Action;
 
 class DoAction extends AbstractCachingAction
 {
+    public function __construct(\Closure $action)
+    {
+        parent::__construct($action);
+    }
+
     public function getActionCode()
     {
         return parent::getActionCode() . "return \$v();";
