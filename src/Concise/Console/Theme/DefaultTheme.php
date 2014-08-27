e7ece415f7c836b780b756edcd603398a3efc008
Elliot Chance <elliotchance@gmail.com>
2014-08-27 23:51:12 +1000

    testThemeHasFailure

--- src/Concise/Console/Theme/DefaultTheme.php
+++ src/Concise/Console/Theme/DefaultTheme.php
@@ -8,6 +8,7 @@ class DefaultTheme
     {
         return [
             'success' => 'green',
+            'failure' => 'red',
         ];
     }
 }
--- tests/Concise/Console/Theme/DefaultThemeTest.php
+++ tests/Concise/Console/Theme/DefaultThemeTest.php
@@ -4,11 +4,17 @@ namespace Concise\Console\Theme;
 
 use \Concise\TestCase;
 
-class DefaultTest extends TestCase
+class DefaultThemeTest extends TestCase
 {
     public function testThemeHasSuccess()
     {
         $theme = new DefaultTheme();
         $this->assert($theme->getTheme(), has_key, 'success');
+    }
+
+    public function testThemeHasFailure()
+    {
+        $theme = new DefaultTheme();
+        $this->assert($theme->getTheme(), has_key, 'failure');
     }
 }
