d9d85124505e2e995bd56302b38d0fa7479bc42f
Elliot Chance <elliotchance@gmail.com>
2014-09-13 16:47:40 +1000

    IsNotABoolean

---
+++ tests/Concise/Matcher/IsNotABooleanTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Concise\Matcher;
+
+class IsNotABooleanTest extends AbstractMatcherTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->matcher = new IsNotABoolean();
+    }
+
+    public function testTrueIsABoolean()
+    {
+        $this->assertFailure(true, is_not_a_boolean);
+    }
+
+    public function testAStringIsNotABoolean()
+    {
+        $this->assert("true", is_not_a_boolean);
+    }
+
+    public function testFalseIsABoolean()
+    {
+        $this->assertFailure(false, is_not_a_boolean);
+    }
+
+    public function testAlternativeShorterSyntax()
+    {
+        $this->assertFailure(true, is_not_a_bool);
+    }
+}
