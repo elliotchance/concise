09d597e9a3e64526c455fac54a34fa51ecbeb92b
Elliot Chance <elliotchance@gmail.com>
2014-09-13 11:38:40 +1000

    testVerboseIsFalseByDefault

--- src/Concise/Console/Command.php
+++ src/Concise/Console/Command.php
@@ -4,13 +4,15 @@ namespace Concise\Console;
 
 use Concise\Console\TestRunner\DefaultTestRunner;
 use Concise\Console\ResultPrinter\ResultPrinterProxy;
+use Concise\Console\ResultPrinter\DefaultResultPrinter;
 
 class Command extends \PHPUnit_TextUI_Command
 {
     protected function createRunner()
     {
+        $resultPrinter = new DefaultResultPrinter();
         $testRunner = new DefaultTestRunner();
-        $testRunner->setPrinter(new ResultPrinterProxy());
+        $testRunner->setPrinter(new ResultPrinterProxy($resultPrinter));
 
         return $testRunner;
     }
--- tests/Concise/Console/CommandTest.php
+++ tests/Concise/Console/CommandTest.php
@@ -11,19 +11,28 @@ class CommandTest extends TestCase
         $this->assert(new Command(), instance_of, 'PHPUnit_TextUI_Command');
     }
 
+    protected function getCommandMock()
+    {
+        return $this->niceMock('Concise\Console\Command')
+                    ->expose('createRunner')
+                    ->done();
+    }
+
     public function testCreateRunnerReturnsAConciseRunner()
     {
-        $command = $this->niceMock('Concise\Console\Command')
-                        ->expose('createRunner')
-                        ->done();
+        $command = $this->getCommandMock();
         $this->assert($command->createRunner(), instance_of, 'Concise\Console\TestRunner\DefaultTestRunner');
     }
 
     public function testPrinterUsesProxy()
     {
-        $command = $this->niceMock('Concise\Console\Command')
-                        ->expose('createRunner')
-                        ->done();
+        $command = $this->getCommandMock();
         $this->assert($command->createRunner()->getPrinter(), instance_of, 'Concise\Console\ResultPrinter\ResultPrinterProxy');
+    }
+
+    public function testVerboseIsFalseByDefault()
+    {
+        $command = $this->getCommandMock();
+        $this->assert($command->createRunner()->getPrinter()->getResultPrinter()->isVerbose(), is_false);
     }
 }
