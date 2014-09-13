aa4979a0e625d55abbfbc05b80cc798ace028838
Elliot Chance <elliotchance@gmail.com>
2014-09-13 16:37:37 +1000

    testTrueIsABoolean

---
+++ src/Concise/Matcher/IsABoolean.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Concise\Matcher;
+
+class IsABoolean extends AbstractMatcher
+{
+    public function supportedSyntaxes()
+    {
+        return array(
+            '? is a boolean' => 'Assert a value is true or false.',
+        );
+    }
+
+    public function match($syntax, array $data = array())
+    {
+        return true;
+    }
+
+    public function getTags()
+    {
+        return array(Tag::BOOLEANS);
+    }
+}
---
+++ tests/Concise/Matcher/IsABooleanTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Concise\Matcher;
+
+class IsABooleanTest extends AbstractMatcherTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->matcher = new IsABoolean();
+    }
+
+    public function testTrueIsABoolean()
+    {
+        $this->assert(true, is_a_boolean);
+    }
+}
