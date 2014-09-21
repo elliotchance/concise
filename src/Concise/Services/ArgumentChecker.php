bc8432c313e271f19564524cee5c6baca3dde8fb
Elliot Chance <elliotchance@gmail.com>
2014-09-21 11:27:33 +1000

    testFailureThrowsInvalidArgumentException

--- src/Concise/Services/ArgumentChecker.php
+++ src/Concise/Services/ArgumentChecker.php
@@ -2,10 +2,18 @@
 
 namespace Concise\Services;
 
+use InvalidArgumentException;
+
 class ArgumentChecker
 {
-    public static function check($value, $types)
+    public static function check($value, $types, $argumentNumber)
     {
-        return true;
+        $checker = new DataTypeChecker();
+
+        try {
+            return $checker->check(array($types), $value);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException("Argument $argumentNumber: " . $e->getMessage());
+        }
     }
 }
--- tests/Concise/Services/ArgumentCheckerTest.php
+++ tests/Concise/Services/ArgumentCheckerTest.php
@@ -6,8 +6,17 @@ use Concise\TestCase;
 
 class ArgumentCheckerTest extends TestCase
 {
-    public function testSuccessReturnsTrue()
+    public function testSuccessReturnsOriginalValue()
     {
-        $this->assert(ArgumentChecker::check(123, 'int'), is_true);
+        $this->assert(ArgumentChecker::check(123, 'int', 1), exactly_equals, 123);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Argument 1: integer not found in string
+     */
+    public function testFailureThrowsInvalidArgumentException()
+    {
+        ArgumentChecker::check(123, 'string', 1);
     }
 }
