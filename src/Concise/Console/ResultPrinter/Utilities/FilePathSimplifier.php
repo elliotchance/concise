42a54f59070178beb028f4bd299562c64e2ce528
Elliot Chance <elliotchance@gmail.com>
2014-09-21 13:55:16 +1000

    testFilePathMustBeAString

--- src/Concise/Console/ResultPrinter/Utilities/FilePathSimplifier.php
+++ src/Concise/Console/ResultPrinter/Utilities/FilePathSimplifier.php
@@ -2,10 +2,18 @@
 
 namespace Concise\Console\ResultPrinter\Utilities;
 
+use Concise\Validation\ArgumentChecker;
+
 class FilePathSimplifier
 {
+    /**
+	 * @param  string $filePath
+	 * @return string
+	 */
     public function process($filePath)
     {
+        ArgumentChecker::check($filePath, 'string', 1);
+
         $cwd = getcwd();
         if (substr($filePath, 0, strlen($cwd)) === $cwd) {
             return substr($filePath, strlen($cwd) + 1);
