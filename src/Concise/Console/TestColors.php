0cfcb3d1c58fad7bdef6c815d5fba9d06a562c77
Elliot Chance <elliot@kounta.com>
2014-09-23 09:21:59 +1000

    Fix syntax error for PHP 5.3

--- src/Concise/Console/TestColors.php
+++ src/Concise/Console/TestColors.php
@@ -13,7 +13,7 @@ class TestColors
         $lines = array(
             $renderer->render('? is null', array(null)),
             $renderer->render('? does not equal ?', array(true, false)),
-            $renderer->render('? has key ? with value ?', array(["foo" => 123], 'foo', 123)),
+            $renderer->render('? has key ? with value ?', array(array("foo" => 123), 'foo', 123)),
             $renderer->render('? is instance of ?', array(new DateTime(), 'DateTime')),
             $renderer->render('? equals ? within ?', array(1.57, 1.5, 0.5)),
             $renderer->render('? is instance of ?', array(function () {}, 'Closure')),
