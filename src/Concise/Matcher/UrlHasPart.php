f01b7026f76922bc638ae8f1c4b47a5a6653e854
Elliot Chance <elliotchance@gmail.com>
2014-10-30 22:19:45 +1100

    testUrl

--- src/Concise/Matcher/UrlHasPart.php
+++ src/Concise/Matcher/UrlHasPart.php
@@ -11,6 +11,7 @@ class UrlHasPart extends AbstractMatcher
             'url ?:string has host ?:string' => 'URL has host.',
             'url ?:string has port ?:int' => 'URL has port.',
             'url ?:string has user ?:string' => 'URL has user.',
+            'url ?:string has password ?:string' => 'URL has password.',
         );
     }
 
@@ -20,14 +21,14 @@ class UrlHasPart extends AbstractMatcher
             'port' => PHP_URL_PORT,
             'host' => PHP_URL_HOST,
             'user' => PHP_URL_USER,
+            'scheme' => PHP_URL_SCHEME,
+            'password' => PHP_URL_PASS,
         ];
         foreach ($parts as $kw => $part) {
             if (strpos($syntax, $kw)) {
                 return parse_url($data[0], $part) == $data[1];
             }
         }
-
-        return parse_url($data[0], PHP_URL_SCHEME) == $data[1];
     }
 
     public function getTags()
--- tests/Concise/Matcher/UrlHasPartTest.php
+++ tests/Concise/Matcher/UrlHasPartTest.php
@@ -10,43 +10,40 @@ class UrlHasPartTest extends AbstractMatcherTestCase
         $this->matcher = new UrlHasPart();
     }
 
-    public function testUrlHasScheme()
+    public function data()
     {
-        $this->assert(url, 'http://google.com', has_scheme, 'http');
+        return [
+            'has scheme 1' => ['http://google.com', 'has scheme', 'http', true],
+            'has scheme 2' => ['http://google.com', 'has scheme', 'https', false],
+            'has scheme 3' => ['google.com', 'has scheme', '', true],
+
+            'has host 1' => ['http://google.com', 'has host', 'google.com', true],
+            'has host 2' => ['http://google.com', 'has host', 'foo.com', false],
+            'has host 3' => ['http://?abc', 'has host', '', true],
+
+            'has port 1' => ['http://foo:123', 'has port', 123, true],
+            'has port 2' => ['http://foo:123', 'has port', 456, false],
+            'has port 3' => ['http://foo:123', 'has port', 0, false],
+
+            'has user 1' => ['http://foo@bar', 'has user', 'foo', true],
+            'has user 2' => ['http://foo@bar', 'has user', 'bar', false],
+            'has user 3' => ['http://bar.com', 'has user', '', true],
+
+            'has pass 1' => ['http://foo:baz@bar', 'has password', 'baz', true],
+            'has pass 2' => ['http://foo:bar@bar', 'has password', 'baz', false],
+            'has pass 3' => ['http://bar.com', 'has password', '', true],
+        ];
     }
 
-    public function testUrlDoesNotHaveScheme()
+    /**
+     * @dataProvider data
+     */
+    public function testUrl($url, $test, $value, $passes)
     {
-        $this->assertFailure(url, 'http://google.com', has_scheme, 'https');
-    }
-
-    public function testUrlHasHost()
-    {
-        $this->assert(url, 'http://google.com', has_host, 'google.com');
-    }
-
-    public function testUrlDoesNotHaveHost()
-    {
-        $this->assertFailure(url, 'http://google.com', has_host, 'foo.com');
-    }
-
-    public function testUrlHasPort()
-    {
-        $this->assert(url, 'http://foo:123', has_port, 123);
-    }
-
-    public function testUrlDoesNotHavePort()
-    {
-        $this->assertFailure(url, 'http://foo:123', has_port, 456);
-    }
-
-    public function testUrlHasUser()
-    {
-        $this->assert(url, 'http://foo@google.com', has_user, 'foo');
-    }
-
-    public function testUrlDoesNotHaveUser()
-    {
-        $this->assertFailure(url, 'http://foo@google.com', has_user, 'bar');
+        if ($passes) {
+            $this->assert(url, $url, $test, $value);
+        } else {
+            $this->assertFailure(url, $url, $test, $value);
+        }
     }
 }
