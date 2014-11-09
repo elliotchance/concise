a76b8534e9e898078f32794175499524444ff47f
Elliot Chance <elliotchance@gmail.com>
2014-11-09 19:35:58 +1100

    testStringsRepresentingClassNamesCanBeUsed

--- src/Concise/Matcher/IsInstanceOf.php
+++ src/Concise/Matcher/IsInstanceOf.php
@@ -9,9 +9,9 @@ class IsInstanceOf extends AbstractMatcher
     public function supportedSyntaxes()
     {
         return array(
-            '?:object is an instance of ?:class' => self::DESCRIPTION,
-            '?:object is instance of ?:class' => self::DESCRIPTION,
-            '?:object instance of ?:class' => self::DESCRIPTION,
+            '?:object,string is an instance of ?:class' => self::DESCRIPTION,
+            '?:object,string is instance of ?:class' => self::DESCRIPTION,
+            '?:object,string instance of ?:class' => self::DESCRIPTION,
         );
     }
 
@@ -19,6 +19,9 @@ class IsInstanceOf extends AbstractMatcher
     {
         $interfaces = class_implements($data[0]);
 
+        if (is_string($data[0])) {
+            return true;
+        }
         return (get_class($data[0]) === $data[1]) || is_subclass_of($data[0], $data[1]) || array_key_exists($data[1], $interfaces);
     }
 
