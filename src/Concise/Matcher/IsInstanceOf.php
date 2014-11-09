5c16fcccbd7acdfafb03ade1c89a62304417ab2b
Elliot Chance <elliotchance@gmail.com>
2014-11-09 19:40:47 +1100

    testClassNameThatDoesntExist

--- src/Concise/Matcher/IsInstanceOf.php
+++ src/Concise/Matcher/IsInstanceOf.php
@@ -17,6 +17,10 @@ class IsInstanceOf extends AbstractMatcher
 
     public function match($syntax, array $data = array())
     {
+        if (!class_exists($data[1])) {
+            throw new DidNotMatchException("No such class '{$data[1]}'.");
+        }
+
         $interfaces = class_implements($data[0]);
 
         if (is_string($data[0])) {
--- tests/Concise/Matcher/IsInstanceOfTest.php
+++ tests/Concise/Matcher/IsInstanceOfTest.php
@@ -34,4 +34,13 @@ class IsInstanceOfTest extends AbstractMatcherTestCase
     {
         $this->assert('\Concise\Matcher\IsInstanceOfTest', instance_of, '\Concise\TestCase');
     }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage No such class 'foobar'.
+     */
+    public function testClassNameThatDoesntExist()
+    {
+        $this->assert('\Concise\Matcher\IsInstanceOfTest', instance_of, '\foobar');
+    }
 }
