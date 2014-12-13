57697630234cb39d979ee24d09801e3506b8fb26
Elliot Chance <elliotchance@gmail.com>
2014-12-13 13:08:32 +1100

    testFinalClassesCanNotBeMocked

--- tests/Concise/Mock/BuilderFinalTest.php
+++ tests/Concise/Mock/BuilderFinalTest.php
@@ -11,6 +11,18 @@ class BuilderFinalTest extends AbstractBuilderTestCase
      * @expectedException \Exception
      * @expectedExceptionMessage is final so it cannot be mocked
      * @dataProvider finalBuilders
+     * @group #129
+     */
+    public function testFinalClassesCanNotBeMocked(MockBuilder $builder)
+    {
+        $builder->get();
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage is final so it cannot be mocked
+     * @dataProvider niceMockBuilders
+     * @group #129
      */
     public function testFinalMethodsCanNotBeMocked(MockBuilder $builder)
     {
