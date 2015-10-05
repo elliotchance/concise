c75117edae8423eb8907700595b06afd013cb651
Elliot Chance <elliotchance@gmail.com>
2015-10-05 15:16:26 +1100

    Minor

--- src/Concise/Extensions/Pho/PhoTestSuiteLoader.php
+++ src/Concise/Extensions/Pho/PhoTestSuiteLoader.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace {
-    if (!class_exists('pho\Runnable\Spec')) {
-        return;
-    }
-}
+//namespace {
+//    if (!class_exists('pho\Runnable\Spec')) {
+//        return;
+//    }
+//}
 
 namespace pho\Reporter {
     use Concise\Core\TestCase;
