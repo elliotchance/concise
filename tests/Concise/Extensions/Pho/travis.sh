feafa106aa5c16e2a68ea875267240c9579aeff9
Elliot Chance <elliot@kounta.com>
2015-10-07 18:22:16 +1100

    Fix trvais expected

--- tests/Concise/Extensions/Pho/travis.sh
+++ tests/Concise/Extensions/Pho/travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CONCISE_BIN="vendor/bin/concise -c tests/Concise/Extensions/Pho/phpunit.xml"
+export CONCISE_BIN="bin/concise -c tests/Concise/Extensions/Pho/phpunit.xml"
 
 # Run a single spec file
 $CONCISE_BIN vendor/danielstjules/pho/spec/Expectation/ExpectationSpec.php
@@ -10,3 +10,16 @@ cat tests/Concise/Extensions/Pho/travis_1.txt
 # Running a folder containing specs
 $CONCISE_BIN --ci --test-suffix=Spec.php vendor/danielstjules/pho/spec > tests/Concise/Extensions/Pho/travis_2.txt
 cat tests/Concise/Extensions/Pho/travis_2.txt
+
+# Make sure the outputs are good
+MD51=`md5 -q tests/Concise/Extensions/Pho/travis_1.txt`
+MD52=`md5 -q tests/Concise/Extensions/Pho/travis_1_expected.txt`
+if [ "$MD51" != "$MD52" ]; then
+    exit 1
+fi
+
+MD51=`md5 -q tests/Concise/Extensions/Pho/travis_2.txt`
+MD52=`md5 -q tests/Concise/Extensions/Pho/travis_2_expected.txt`
+if [ "$MD51" != "$MD52" ]; then
+    exit 2
+fi
