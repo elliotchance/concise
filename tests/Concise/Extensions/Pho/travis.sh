2b2aba9313b88d406c54446226e49de5b310903b
Elliot Chance <elliot@kounta.com>
2015-10-07 18:05:25 +1100

    Travis script

--- tests/Concise/Extensions/Pho/travis.sh
+++ tests/Concise/Extensions/Pho/travis.sh
@@ -5,6 +5,8 @@ export CONCISE_BIN="vendor/bin/concise -c tests/Concise/Extensions/Pho/phpunit.x
 # Run a single spec file
 $CONCISE_BIN vendor/danielstjules/pho/spec/Expectation/ExpectationSpec.php
 $CONCISE_BIN --ci vendor/danielstjules/pho/spec/Expectation/ExpectationSpec.php > tests/Concise/Extensions/Pho/travis_1.txt
+cat tests/Concise/Extensions/Pho/travis_1.txt
 
 # Running a folder containing specs
 $CONCISE_BIN --ci --test-suffix=Spec.php vendor/danielstjules/pho/spec > tests/Concise/Extensions/Pho/travis_2.txt
+cat tests/Concise/Extensions/Pho/travis_2.txt
