4fb7569c74e3be14cd659e37ef3dcd44eeae3fbf
Elliot Chance <elliotchance@gmail.com>
2015-10-08 23:46:40 +1100

    Chop expected

--- tests/Concise/Extensions/Pho/travis.sh
+++ tests/Concise/Extensions/Pho/travis.sh
@@ -5,11 +5,11 @@ set -x
 export CONCISE_BIN="bin/concise -c tests/Concise/Extensions/Pho/phpunit.xml"
 
 # Run a single spec file
-$CONCISE_BIN --ci vendor/danielstjules/pho/spec/Expectation/ExpectationSpec.php > tests/Concise/Extensions/Pho/travis_1.txt
+$CONCISE_BIN --ci vendor/danielstjules/pho/spec/Expectation/ExpectationSpec.php | cut -c65- > tests/Concise/Extensions/Pho/travis_1.txt
 cat tests/Concise/Extensions/Pho/travis_1.txt
 
 # Running a folder containing specs
-$CONCISE_BIN --ci --test-suffix=Spec.php vendor/danielstjules/pho/spec > tests/Concise/Extensions/Pho/travis_2.txt
+$CONCISE_BIN --ci --test-suffix=Spec.php vendor/danielstjules/pho/spec | cut -c65- > tests/Concise/Extensions/Pho/travis_2.txt
 cat tests/Concise/Extensions/Pho/travis_2.txt
 
 # Make sure the outputs are good
