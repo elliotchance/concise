9b00ae3589c78eb772b8e58f55ed43fb1c0d8bfb
Elliot Chance <elliotchance@gmail.com>
2015-10-18 20:53:01 +1100

    testWillThrowExceptionIfTheCustomNameAlreadyExists

--- tests/Concise/Mock/BuilderCustomClassNameTest.php
+++ tests/Concise/Mock/BuilderCustomClassNameTest.php
@@ -2,6 +2,10 @@
 
 namespace Concise\Mock;
 
+class FooClass
+{
+}
+
 /**
  * @group mocking
  */
@@ -26,5 +30,16 @@ class BuilderCustomClassNameTest extends AbstractBuilderTestCase
         $rand = "Concise\\Mock\\Temp" . md5(rand());
         $mock = $builder->setCustomClassName($rand)->get();
         $this->assert(get_class($mock))->equals($rand);
+    }
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage You cannot use 'Concise\Mock\FooClass' because a class with that name already exists.
+     * @dataProvider allBuilders
+     * @group #304
+     */
+    public function testWillThrowExceptionIfTheCustomNameAlreadyExists(
+        MockBuilder $builder
+    ) {
+        $builder->setCustomClassName('Concise\Mock\FooClass')->get();
     }
 }
